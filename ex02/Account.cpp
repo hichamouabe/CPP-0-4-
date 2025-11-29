d78a41a625395917b673333ad34f33730bf14727
NAAR <naar@example.com>
2025-11-29 17:05:16 +0100

    7ta ga3 l cluster , db mafiyach

---
+++ ex02/Account.cpp
@@ -0,0 +1,11 @@
+#include "Account.hpp"
+#include <iostram>
+#include <iomanip>
+#include <ctime>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+
